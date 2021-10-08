b76e53695df9fb1c2a1d27fce7ea3415a41b2b4e
B. David Nejand <dnejand@gmail.com>
2021-10-08 15:58:27 -0400

    Initial Commit

---
+++ 1-Values/2-Play with Values/B-Naming and Identifiers/Naming and Identifiers.playground/Pages/Programmer Talk.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+-let numberOfDogs = 101
+/*:
+When you define a constant in Swift, it’s called _declaring_ a constant. So the statement above is known as a _declaration_. The instruction it gives to the computer is “Declare that the name `numberOfDogs` has the value `101`.”
+
+The word `let` is an example of a _keyword_. Each keyword has a special meaning in Swift and can't be used as a name.  The `let` keyword is used to declare a constant. Over time, you’ll be introduced to more Swift keywords.
+
+The last part of the statement, `= 101`, is where you _assign_ a value to the constant. This is called _assignment_ and the equal sign `=` is called the _assignment operator_.
+
+ - Note: Don’t worry about memorizing every new term - it can be overwhelming! You’ll see these terms used in context during the rest of the course, and you can always find them in the Glossary if you need to refresh your memory.
+
+[Previous](@previous)  |  page 8 of 14  |  [Next: Wrapping Up](@next)
+ */
